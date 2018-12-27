2cac8a4f45511aad53101fffef5bf6bf00f42f0c
Eric <smart032410@gmail.com>
2018-12-27 13:43:51 +0800

    Follow event

---
+++ follow.py
@@ -0,0 +1,27 @@
+from linebot.models import (BoxComponent, BubbleContainer, ButtonComponent,
+                            FlexSendMessage, TextComponent)
+
+
+def follow_message(line_user_id):
+    bubble_template = BubbleContainer(
+        body=BoxComponent(
+            layout='vertical',
+            contents=[
+                TextComponent(
+                    text='歡迎加入',
+                    wrap=True,
+                    weight= 'bold',
+                    size='lg',
+                ),
+                TextComponent(
+                    text='您好，我是 Lassie！我致力於打造一個連結各種設備與用戶之間聰明、可靠、友善的互動系統，讓用戶能隨時掌握設備狀況建立連結！第一次使用請先完成綁定喔！',
+                    wrap=True,
+                    size='md',
+                    margin='md'
+                )
+            ]
+        )
+    )
+    message = FlexSendMessage(
+        alt_text='抱歉，我聽不懂指令', contents=bubble_template)
+    return message
--- richmenu.py
+++ richmenu.py
@@ -10,6 +10,9 @@ app.config.from_object(config)
 
 line_bot_api = LineBotApi(app.config['LINE_CHANNEL_ACCESS_TOKEN'])
 
+def unlink_rm(line_user_id):
+    line_bot_api.unlink_rich_menu_from_user(line_user_id)
+
 def link_rm_to_user(line_user_id):
     '''
     Link rich menu to user
