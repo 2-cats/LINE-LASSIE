b0bf8977c6b0eae8f811fd7ca2caf2e5d2feb24e
Eric <smart032410@gmail.com>
2018-12-27 11:50:58 +0800

    Handle exception message

---
+++ contact.py
@@ -0,0 +1,4 @@
+from linebot.models import TextSendMessage
+
+def contact_us(line_user_id):
+    return TextSendMessage(text='功能開發中...')
---
+++ device.py
@@ -0,0 +1,4 @@
+from linebot.models import TextSendMessage
+
+def device_list(line_user_id):
+    return TextSendMessage(text='功能開發中...')
