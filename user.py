ede3dd71690abfc0a06e6b5a6acb1a6fb6a3842e
Eric <smart032410@gmail.com>
2018-12-27 13:29:58 +0800

    Username and line_user_id convert

---
+++ user.py
@@ -0,0 +1,19 @@
+from rds import connect
+
+def username_to_line_user_id(username):
+    database = connect()
+    cursor = database.cursor()
+    args = (username,)
+    cursor.execute("SELECT line_user_id FROM users WHERE aws_username =  %s", args)
+    result = cursor.fetchone()
+    database.close()
+    return result[0]
+
+def line_user_id_to_username(line_user_id):
+    database = connect()
+    cursor = database.cursor()
+    args = (line_user_id,)
+    cursor.execute("SELECT aws_username FROM users WHERE line_user_id =  %s", args)
+    result = cursor.fetchone()
+    database.close()
+    return result[0]
