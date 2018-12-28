bdce11b84eb4a4b0bb289ce7ecf4bb95336299cf
Eric <smart032410@gmail.com>
2018-12-28 18:04:08 +0800

    Env config

---
+++ config.py
@@ -0,0 +1,54 @@
+from flask import Flask
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile('config.py')
+
+class Config:
+    DB_HOST = app.config['DB_HOST']
+    DB_USERNAME = app.config['DB_USERNAME']
+    DB_PASSWORD = app.config['DB_PASSWORD']
+    DB_NAME = app.config['DB_NAME']
+    DB_PORT = app.config['DB_PORT']
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(
+        Config.DB_USERNAME,
+        Config.DB_PASSWORD,
+        Config.DB_HOST,
+        Config.DB_PORT,
+        Config.DB_NAME
+    )
+
+class TestingConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(
+        Config.DB_USERNAME,
+        Config.DB_PASSWORD,
+        Config.DB_HOST,
+        Config.DB_PORT,
+        Config.DB_NAME
+    )
+
+class ProductionConfig(Config):
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(
+        Config.DB_USERNAME,
+        Config.DB_PASSWORD,
+        Config.DB_HOST,
+        Config.DB_PORT,
+        Config.DB_NAME
+    )
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
