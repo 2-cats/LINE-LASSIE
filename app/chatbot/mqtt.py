6cc9aaaada8d6dadd250a2a26344360be7fb39f5
tommy <tommy.chen@goodlinker.io>
2019-01-04 09:13:39 +0800

    字串修正

--- app/chatbot/mqtt.py
+++ app/chatbot/mqtt.py
@@ -16,25 +16,25 @@ def lassie_alarm_message(mqtt_message):
     rule = ''
     if mqtt_message['t'] == 'counter':
         unit = ' (次)'
-        rule = '設定'+' : '+str(mqtt_message['r'])
+        rule = '設定 : ' + str(mqtt_message['r'])
     if mqtt_message['t'] == 'lamp' or 'state' or 'color' or 'detector':
         unit = ' (偵測值)'
-        rule = '設定' + ' : ' + str(mqtt_message['r'])
+        rule = '設定 : ' + str(mqtt_message['r'])
     if mqtt_message['t'] == 'current':
         unit = ' (mAh)'
-        rule = '設定' + ' : ' + str(mqtt_message['r'])
+        rule = '設定 : ' + str(mqtt_message['r'])
     if mqtt_message['t'] == 'temperature':
         unit = ' (℃)'
-        rule = '上下限為' + ' : ' + str(mqtt_message['ra']['h']) + " ～ " + str(mqtt_message['ra']['l'])
+        rule = '上下限為 : ' + str(mqtt_message['ra']['h']) + " ～ " + str(mqtt_message['ra']['l'])
     if mqtt_message['t'] == 'humidity':
         unit = ' (%)'
-        rule = '上下限為' + ' : ' + str(mqtt_message['ra']['h']) + " ～ " + str(mqtt_message['ra']['l'])
+        rule = '上下限為 : ' + str(mqtt_message['ra']['h']) + " ～ " + str(mqtt_message['ra']['l'])
     if mqtt_message['t'] == 'timer':
         unit = ' (秒)'
-        rule = '設定' + ' : ' + str(mqtt_message['r'])
+        rule = '設定 : ' + str(mqtt_message['r'])
     if mqtt_message['t'] == 'ocr':
         unit = '(偵測值)'
-        rule = '上下限為' + ' : ' + str(mqtt_message['ra']['h']) + " ～ " + str(mqtt_message['ra']['l'])
+        rule = '上下限為 : ' + str(mqtt_message['ra']['h']) + " ～ " + str(mqtt_message['ra']['l'])
     message_list = []
     if mqtt_message['url'] != "":
         message = ImageSendMessage(
