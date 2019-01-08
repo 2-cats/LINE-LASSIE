d02d48d886e7e681beae6ebd446fd9468dc5f8eb
Eric <smart032410@gmail.com>
2019-01-08 15:01:03 +0800

    Test: contact

--- tests/test_bind.py
+++ tests/test_bind.py
@@ -2,7 +2,7 @@ import unittest
 
 from app.chatbot.bind import check_bind
 
-class BasicsTestCase(unittest.TestCase):
+class BindTestCase(unittest.TestCase):
     def setUp(self):
         self.bind_user_lind_id = "Ud70573ef58ee136041925101bc130d72"
         self.unbind_user_lind_id = "line_user_id"
