56b6c4b0ee91b114bd9e5fc03e1ff3218e9a400f
Eric <smart032410@gmail.com>
2019-01-08 17:36:34 +0800

    Test: error message

--- app/models.py
+++ app/models.py
@@ -1,9 +1,7 @@
 from datetime import datetime
 
 from flask import Flask
-from flask_login import AnonymousUserMixin, UserMixin
 from linebot import LineBotApi
-from werkzeug.security import check_password_hash, generate_password_hash
 
 import config
 
@@ -14,7 +12,7 @@ app.config.from_pyfile('config.py')
 
 line_bot_api = LineBotApi(app.config['LINE_CHANNEL_ACCESS_TOKEN'])
 
-class User(UserMixin, db.Model):
+class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     line_user_id = db.Column(db.String(64), unique=True)
