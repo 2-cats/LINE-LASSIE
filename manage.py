948345d61ced98bfd59da5f6b26d64eeae118a2d
Ubuntu <ubuntu@ip-172-31-20-212.ap-southeast-1.compute.internal>
2019-03-13 06:36:41 +0000

    Get env flask config

--- manage.py
+++ manage.py
@@ -13,6 +13,12 @@ if os.environ.get('FLASK_COVERAGE'):
     COV = coverage.coverage(branch=True, include='app/*')
     COV.start()
 
+if os.path.exists('.env'):
+    for line in open('.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
+
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 manager = Manager(app)
