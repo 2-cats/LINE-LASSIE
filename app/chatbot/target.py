187cc87b6e5d98e5e3d2c9a93bb8ec49fc3e933c
Eric <smart032410@gmail.com>
2019-06-25 10:32:05 +0800

    Fix target push none id

--- app/chatbot/target.py
+++ app/chatbot/target.py
@@ -33,7 +33,7 @@ def get_push_id(username):
     cursor.execute("SELECT source_id FROM users JOIN members ON users.id = members.user_id WHERE users.aws_user_name = %s ORDER BY members.id DESC", args)
     member_result = cursor.fetchone()
     if member_result is None:
-        cursor.execute("SELECT line_user_id FROM users WHERE aws_user_name = %s", args)
+        cursor.execute("SELECT line_user_id FROM users WHERE aws_user_name = %s and deleted_at=None", args)
         user_result = cursor.fetchone()
         psuh_id = user_result[0]
     else:
