b7e238f280a0e968f946a2bd07cf21a5d22d7231
Eric <smart032410@gmail.com>
2019-07-16 10:29:55 +0800

    Fix get_push_id

--- app/chatbot/target.py
+++ app/chatbot/target.py
@@ -29,14 +29,7 @@ def get_push_id(username):
         charset="utf8"
     )
     cursor = database.cursor()
-    args = (username,)
-    cursor.execute("SELECT source_id FROM users JOIN members ON users.id = members.user_id WHERE users.aws_user_name = %s ORDER BY members.id DESC", args)
-    member_result = cursor.fetchone()
-    if member_result is None:
-        cursor.execute("SELECT line_user_id FROM users WHERE aws_user_name = %s and deleted_at=None", args)
-        user_result = cursor.fetchone()
-        psuh_id = user_result[0]
-    else:
-        psuh_id = member_result[0]
+    cursor.execute("SELECT line_user_id FROM users WHERE aws_user_name = %s and deleted_at=None", args)
+    user_result = cursor.fetchone()
     database.close()
-    return psuh_id
+    return user_result[0]
